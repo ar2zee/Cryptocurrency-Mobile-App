6551fb043dc53e4b9622113e95e556f58f701560
Oganesyan Artur <arturris@mail.ru>
2018-09-11 23:39:14 -0500

    Init Commit

---
+++ App.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import { View } from 'react-native';
+import { Header } from './src/components';
+
+class App extends Component {
+  render() {
+    return (
+      <View>
+        <Header />
+      </View>
+    );
+  }
+}
+
+
+
+export default App;
