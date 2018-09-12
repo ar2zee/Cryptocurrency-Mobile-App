b85e37580e5f1db1687992def0500ccb3143f573
Oganesyan Artur <arturris@mail.ru>
2018-09-12 17:56:21 -0500

    Working store and remote dev tools

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
 import React, {Component} from 'react';
 import { View } from 'react-native';
-import { Header } from './src/components';
+import {Provider} from 'react-redux';
+
+import { Header, CryptoContainer } from './src/components';
+import Store from './src/Store.js';
 
 class App extends Component {
   render() {
     return (
-      <View>
-        <Header />
-      </View>
+      <Provider store={Store} >
+        <View>
+          <Header />
+          <CryptoContainer />
+        </View>
+      </Provider>
     );
   }
 }
